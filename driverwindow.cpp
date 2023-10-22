26882808ebd85da2251d6488e9aff472a2f7dd1d
A-KRY <AliothKRY@foxmail.com>
2023-10-22 18:58:01 +0800

    添加USB端口选择和MIDI通道选择。

--- driverwindow.cpp
+++ driverwindow.cpp
@@ -2,23 +2,112 @@
 #include "./ui_driverwindow.h"
 
 #define MAX_SYSEX_BUFFER 65535
+#define DEFAULT_PORT_NAME "(Not Connected)"
+#define INFO(TITLE, TEXT) QMessageBox::information(this, (TITLE), (TEXT), QMessageBox::Ok);
 
 DriverWindow::DriverWindow(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::DriverWindow)
     , midiPort(virtualMIDICreatePortEx2(L"Nano Breath Controller", nullptr, 0, MAX_SYSEX_BUFFER, TE_VM_FLAGS_INSTANTIATE_BOTH))
-//    midiOut(new RtMidiOut(RtMidi::Api::WINDOWS_MM))
+    , usbPortUpdateTimer(new QTimer(this))
+    , currentMidiChannel(0)
+    , currentSerialPort(new QSerialPort())
 {
     ui->setupUi(this);
-//    midiOut->openVirtualPort("NanoBreathController");
+
+    // Available USB port update
+    // 可用 USB 端口更新
+    usbPortUpdateTimer->setInterval(5000);
+    connect(usbPortUpdateTimer, &QTimer::timeout, this, &DriverWindow::updateAvailableUsbPort);
+    this->updateAvailableUsbPort();
+    usbPortUpdateTimer->start();
+
+    // midiChannelComboBox initialization
+    // 初始化 midiChannelComboBox
+    for (auto i = 1; i <= 16; ++i) {
+        ui->midiChannelComboBox->addItem(QString::number(i));
+    }
+
+    // availableUsbPortComboBox Callbacks
+    // availableUsbPortComboBox 回调函数
+    connect(ui->availableUsbPortComboBox, &QComboBox::activated,
+            this, &DriverWindow::availableUsbPortComboBox_onActivated);
+
+    // midiChannelComboBox Callbacks
+    // midiChannelComboBox 回调函数
+    connect(ui->midiChannelComboBox, &QComboBox::activated,
+            this, &DriverWindow::midiChannelComboBox_onActivated);
 }
 
 DriverWindow::~DriverWindow()
 {
-//    midiOut->closePort();
     if (midiPort) {
         virtualMIDIClosePort(midiPort);
     }
+    if (currentSerialPort->isOpen()) {
+        currentSerialPort->close();
+    }
     delete ui;
 }
+
+void DriverWindow::availableUsbPortComboBox_onActivated(int index) {
+    // If port not changed, do nothing and return
+    // 如果串口未变，直接返回
+    if (currentSerialPort->portName() == ui->availableUsbPortComboBox->currentText()) {
+        return;
+    }
+
+    // Close serial port if it's opened
+    // 若串口已打开，则关闭
+    if (currentSerialPort->portName() != ui->availableUsbPortComboBox->currentText()) {
+        if (currentSerialPort->isOpen()) {
+            currentSerialPort->close();
+            currentSerialPort->setPortName(DEFAULT_PORT_NAME);
+        }
+    }
+
+    // Open serial port only when selected item isn't "(Not Connected)"
+    // 只有选中项不为 "(Not Connected)" 时才打开端口
+    if (index != 0) {
+        // Set port name and open port
+        // 设置端口名并打开端口
+        currentSerialPort->setPortName(ui->availableUsbPortComboBox->currentText());
+
+        if (currentSerialPort->open(QIODevice::ReadWrite)) {
+            currentSerialPort->setBaudRate(QSerialPort::Baud2400);
+            return;
+        }
+        else {
+            QMessageBox::information(this, "Error", "串口打开失败", QMessageBox::Ok);
+            ui->availableUsbPortComboBox->setCurrentIndex(0);
+            currentSerialPort->setPortName(DEFAULT_PORT_NAME);
+        }
+    }
+}
+
+void DriverWindow::midiChannelComboBox_onActivated(int index) {
+    currentMidiChannel = index;
+}
+
+void DriverWindow::updateAvailableUsbPort() {
+    QString previousText = ui->availableUsbPortComboBox->currentText();
+//    QMessageBox::information(this, "info", previousText, QMessageBox::Ok);
+
+    ui->availableUsbPortComboBox->clear();
+    ui->availableUsbPortComboBox->addItem(DEFAULT_PORT_NAME);
+    const auto availablePorts = QSerialPortInfo::availablePorts();
+    for (auto& portInfo : availablePorts) {
+        ui->availableUsbPortComboBox->addItem(portInfo.portName());
+    }
+
+    // If previous port still exists, set selected item to it,
+    // otherwise set to 0 which corresponding "(Not Connected)"
+    // 若之前的端口还存在，将其设为当前选项。否则设为 0 即 "(Not Connected)"
+    auto index = ui->availableUsbPortComboBox->findText(previousText);
+    ui->availableUsbPortComboBox->setCurrentIndex(
+            index == -1 ? 0 : index
+            );
+}
+
+
 
--- driverwindow.h
+++ driverwindow.h
@@ -2,8 +2,12 @@
 #define DRIVERWINDOW_H
 
 #include <QWidget>
-// #include "deprecated/RtMidi.h"
 #include "teVirtualMIDI.h"
+#include <QSerialPortInfo>
+#include <QTimer>
+#include <QSerialPort>
+#include <QMessageBox>
+#include <iostream>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class DriverWindow; }
@@ -13,15 +17,57 @@ class DriverWindow : public QWidget
 {
     Q_OBJECT
 
-//    std::unique_ptr<RtMidiOut> midiOut;
+    /**
+     * @author  A-KRY
+     * @brief   <p>虚拟 MIDI 端口</p>
+     * <p>Virtual MIDI Port</p>
+     * @date    2023-10-20
+     */
     LPVM_MIDI_PORT midiPort;
-//    std::unique_ptr<VM_MIDI_PORT> midiPort;
+
+    /**
+     * @brief   <p>USB 端口更新定时器</p>
+     * <p>Timer for updating available USB port.</p>
+     * @author  A-KRY
+     * @date    2023/10/22 14:07
+     */
+    QTimer* usbPortUpdateTimer;
+
+    BYTE currentMidiChannel;
+
+    QSerialPort* currentSerialPort;
 
 public:
-    DriverWindow(QWidget *parent = nullptr);
-    ~DriverWindow();
+    explicit DriverWindow(QWidget *parent = nullptr);
+    ~DriverWindow() override;
 
 private:
     Ui::DriverWindow *ui;
+
+    /**
+     * @brief   <p>更新 availableUsbPortComboBox 的选项</p>
+     * <p> Update items in availableUsbPortComboBox</p>
+     * @author  A-KRY
+     * @date    2023/10/21 17:36
+     */
+    void updateAvailableUsbPort();
+    
+    /**
+     * @brief   <p>availableUsbPortComboBox 选中项变更的回调函数</p>
+     * <p>availableUsbPortComboBox current index changed callback.</p>
+     * @author  A-KRY
+     * @date    2023/10/22 14:48
+     */
+     void availableUsbPortComboBox_onActivated(int index);
+     
+     /**
+      * @brief   <p>midiChannelComboBox 选中项变更的回调函数</p>
+      * <p>midiChannelComboBox current index changed callback.</p>
+      * @author  A-KRY
+      * @date    2023/10/22 14:52
+      */
+      void midiChannelComboBox_onActivated(int index);
+
+protected:
 };
 #endif // DRIVERWINDOW_H
