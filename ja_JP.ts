038c4661445662ab050dea7378ae3a2e4a722e06
A-KRY <AliothKRY@foxmail.com>
2023-10-28 13:10:38 +0800

    添加其他文件

---
+++ ja_JP.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>DriverWindow</name>
+    <message>
+        <location filename="driverwindow.ui" line="14"/>
+        <source>DriverWindow</source>
+        <translation type="unfinished">ドライバーウィンドウ</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.ui" line="61"/>
+        <location filename="driverwindow.cpp" line="40"/>
+        <location filename="driverwindow.cpp" line="523"/>
+        <source>USB Port</source>
+        <translation type="unfinished">USB ポート</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.ui" line="93"/>
+        <location filename="driverwindow.cpp" line="42"/>
+        <location filename="driverwindow.cpp" line="524"/>
+        <source>MIDI Channel</source>
+        <translation type="unfinished">MIDI チャネル</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.ui" line="119"/>
+        <location filename="driverwindow.cpp" line="41"/>
+        <location filename="driverwindow.cpp" line="525"/>
+        <source>CC</source>
+        <translation type="unfinished">CC</translation>
+    </message>
+    <message>
+        <source>CC Type</source>
+        <translation type="obsolete">CC タイプ</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.ui" line="148"/>
+        <location filename="driverwindow.cpp" line="204"/>
+        <location filename="driverwindow.cpp" line="239"/>
+        <location filename="driverwindow.cpp" line="330"/>
+        <location filename="driverwindow.cpp" line="526"/>
+        <source>Smoothness</source>
+        <translation type="unfinished">平滑度</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.cpp" line="39"/>
+        <location filename="driverwindow.cpp" line="522"/>
+        <source>Nano Breath Controller USB Driver</source>
+        <translation type="unfinished">Nano Breath Controller USB Driver</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.cpp" line="86"/>
+        <location filename="driverwindow.cpp" line="104"/>
+        <location filename="driverwindow.cpp" line="501"/>
+        <source>(Not Connected)</source>
+        <translation type="unfinished">（未接続）</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.cpp" line="102"/>
+        <source>Error</source>
+        <translation type="unfinished">エラー</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.cpp" line="102"/>
+        <source>Serial port open failed.</source>
+        <translation type="unfinished">シリアルポートのオープンに失敗しました。</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="78"/>
+        <location filename="main.cpp" line="111"/>
+        <source>Exit</source>
+        <translation type="unfinished">終了</translation>
+    </message>
+</context>
+</TS>
---
+++ zh_CN.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>DriverWindow</name>
+    <message>
+        <location filename="driverwindow.ui" line="14"/>
+        <source>DriverWindow</source>
+        <translation type="unfinished">驱动窗口</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.ui" line="61"/>
+        <location filename="driverwindow.cpp" line="40"/>
+        <location filename="driverwindow.cpp" line="523"/>
+        <source>USB Port</source>
+        <translation type="unfinished">USB 端口</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.ui" line="93"/>
+        <location filename="driverwindow.cpp" line="42"/>
+        <location filename="driverwindow.cpp" line="524"/>
+        <source>MIDI Channel</source>
+        <translation type="unfinished">MIDI 通道</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.ui" line="119"/>
+        <location filename="driverwindow.cpp" line="41"/>
+        <location filename="driverwindow.cpp" line="525"/>
+        <source>CC</source>
+        <translation type="unfinished">CC</translation>
+    </message>
+    <message>
+        <source>CC Type</source>
+        <translation type="obsolete">CC 类型</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.ui" line="148"/>
+        <location filename="driverwindow.cpp" line="204"/>
+        <location filename="driverwindow.cpp" line="239"/>
+        <location filename="driverwindow.cpp" line="330"/>
+        <location filename="driverwindow.cpp" line="526"/>
+        <source>Smoothness</source>
+        <translation type="unfinished">平滑度</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.cpp" line="39"/>
+        <location filename="driverwindow.cpp" line="522"/>
+        <source>Nano Breath Controller USB Driver</source>
+        <translation type="unfinished">Nano Breath Controller USB Driver</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.cpp" line="86"/>
+        <location filename="driverwindow.cpp" line="104"/>
+        <location filename="driverwindow.cpp" line="501"/>
+        <source>(Not Connected)</source>
+        <translation type="unfinished">（未连接）</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.cpp" line="102"/>
+        <source>Error</source>
+        <translation type="unfinished">错误</translation>
+    </message>
+    <message>
+        <location filename="driverwindow.cpp" line="102"/>
+        <source>Serial port open failed.</source>
+        <translation type="unfinished">串口打开失败。</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="78"/>
+        <location filename="main.cpp" line="111"/>
+        <source>Exit</source>
+        <translation type="unfinished">退出</translation>
+    </message>
+</context>
+</TS>
