e08e635b7c51f21c47f1afd2f1a9c90ad7826ba0
A-KRY <AliothKRY@foxmail.com>
2023-11-02 08:59:54 +0800

    添加.ts文件

---
+++ Resource/Translations/en_US.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DriverWindow</name>
+    <message>
+        <location filename="../../Src/driverwindow.ui" line="14"/>
+        <source>DriverWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.ui" line="61"/>
+        <location filename="../../Src/driverwindow.cpp" line="38"/>
+        <location filename="../../Src/driverwindow.cpp" line="536"/>
+        <source>USB Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.ui" line="93"/>
+        <location filename="../../Src/driverwindow.cpp" line="40"/>
+        <location filename="../../Src/driverwindow.cpp" line="537"/>
+        <source>MIDI Channel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.ui" line="119"/>
+        <location filename="../../Src/driverwindow.cpp" line="39"/>
+        <location filename="../../Src/driverwindow.cpp" line="538"/>
+        <source>CC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.ui" line="148"/>
+        <location filename="../../Src/driverwindow.cpp" line="203"/>
+        <location filename="../../Src/driverwindow.cpp" line="238"/>
+        <location filename="../../Src/driverwindow.cpp" line="329"/>
+        <location filename="../../Src/driverwindow.cpp" line="539"/>
+        <source>Smoothness</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.cpp" line="37"/>
+        <location filename="../../Src/driverwindow.cpp" line="535"/>
+        <source>Nano Breath Controller USB Driver</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.cpp" line="84"/>
+        <location filename="../../Src/driverwindow.cpp" line="102"/>
+        <location filename="../../Src/driverwindow.cpp" line="490"/>
+        <location filename="../../Src/driverwindow.cpp" line="517"/>
+        <source>(Not Connected)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.cpp" line="100"/>
+        <location filename="../../Src/driverwindow.cpp" line="488"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.cpp" line="100"/>
+        <location filename="../../Src/driverwindow.cpp" line="488"/>
+        <source>Serial port open failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../Src/main.cpp" line="76"/>
+        <location filename="../../Src/main.cpp" line="109"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ Resource/Translations/ja_JP.ts
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP" sourcelanguage="en_US">
+<context>
+    <name>DriverWindow</name>
+    <message>
+        <location filename="../../Src/driverwindow.ui" line="14"/>
+        <source>DriverWindow</source>
+        <translation type="unfinished">DriverWindow</translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.ui" line="61"/>
+        <location filename="../../Src/driverwindow.cpp" line="38"/>
+        <location filename="../../Src/driverwindow.cpp" line="536"/>
+        <source>USB Port</source>
+        <translation type="unfinished">USB ポート</translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.ui" line="93"/>
+        <location filename="../../Src/driverwindow.cpp" line="40"/>
+        <location filename="../../Src/driverwindow.cpp" line="537"/>
+        <source>MIDI Channel</source>
+        <translation type="unfinished">MIDI チャネル</translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.ui" line="119"/>
+        <location filename="../../Src/driverwindow.cpp" line="39"/>
+        <location filename="../../Src/driverwindow.cpp" line="538"/>
+        <source>CC</source>
+        <translation type="unfinished">CC</translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.ui" line="148"/>
+        <location filename="../../Src/driverwindow.cpp" line="203"/>
+        <location filename="../../Src/driverwindow.cpp" line="238"/>
+        <location filename="../../Src/driverwindow.cpp" line="329"/>
+        <location filename="../../Src/driverwindow.cpp" line="539"/>
+        <source>Smoothness</source>
+        <translation type="unfinished">平滑度</translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.cpp" line="37"/>
+        <location filename="../../Src/driverwindow.cpp" line="535"/>
+        <source>Nano Breath Controller USB Driver</source>
+        <translation type="unfinished">Nano Breath Controller USB Driver</translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.cpp" line="84"/>
+        <location filename="../../Src/driverwindow.cpp" line="102"/>
+        <location filename="../../Src/driverwindow.cpp" line="490"/>
+        <location filename="../../Src/driverwindow.cpp" line="517"/>
+        <source>(Not Connected)</source>
+        <translation type="unfinished">（未接続）</translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.cpp" line="100"/>
+        <location filename="../../Src/driverwindow.cpp" line="488"/>
+        <source>Error</source>
+        <translation type="unfinished">エラー</translation>
+    </message>
+    <message>
+        <location filename="../../Src/driverwindow.cpp" line="100"/>
+        <location filename="../../Src/driverwindow.cpp" line="488"/>
+        <source>Serial port open failed.</source>
+        <translation type="unfinished">シリアルポートのオープンに失敗しました。</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../Src/main.cpp" line="76"/>
+        <location filename="../../Src/main.cpp" line="109"/>
+        <source>Exit</source>
+        <translation type="unfinished">終了</translation>
+    </message>
+</context>
+</TS>
